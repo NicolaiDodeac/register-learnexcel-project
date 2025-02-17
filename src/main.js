8d17d0736a296770ac5d22836838743f849c0a5b
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2025-02-17 20:34:36 +0000

    finish

--- src/main.js
+++ src/main.js
@@ -7,5 +7,5 @@ handleFormSubmit('registration-form');
 handleFormSubmit('modal-registration-form');
 
 document.addEventListener('DOMContentLoaded', () => {
-  startCountdown('countdown-timer', '2025-02-14T23:59:59');
+  startCountdown('countdown-timer', '2025-03-01T23:59:59');
 });
